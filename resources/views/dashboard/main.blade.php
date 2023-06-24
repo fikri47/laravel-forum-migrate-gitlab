aa5b8efb98605f57ae10571cf6cc8add3e23ef25
fikri47 <fadillahfikri48@gmail.com>
2023-06-24 13:24:27 +0700

    fix

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -111,9 +111,9 @@
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; {{ now()->year }} <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{ now()->year }} <a href="#">FOURDI</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
