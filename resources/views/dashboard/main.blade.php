fc1c77be3f8a54ae2143e8887fd986037a930327
fikri47 <fadillahfikri48@gmail.com>
2023-06-24 22:53:48 +0700

    fix sidebar

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -28,7 +28,7 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4 position-fixed" >
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="{{asset("image/logo_chat_putih-removebg-preview.png")}}" alt="Fourdi" class="brand-image img-circle elevation-3" style="opacity: .8">
